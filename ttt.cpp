68cd4504861e6b73aaa4608cecfc824d7acba44c
john <john@poseidonos.com>
2021-10-03 11:18:53 -0700

    Test Branch

--- ttt.cpp
+++ ttt.cpp
@@ -3,13 +3,18 @@
 #include <vector>
 #include <string>
 
-//#include "ttt_functions.hpp"
+// need to include this or the functions you wrote won't be included
+#include "ttt_functions.hpp"
 void board();
 
 using std::cout;
 using std::cin;
 
 bool player1_turn = true;
+//a const char is basially a C version of a C++ string so you should be able to use
+//string player1 = "C"
+//also the double "" is what you want or else this 'X' is an ascii value meaning
+//some value like 45
 
 const char player1 = 'X';
 const char player2 = 'O';
